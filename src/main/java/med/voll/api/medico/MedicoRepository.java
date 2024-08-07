2a9de6017b0c53673a0b76b44962c2fc72715401
João Pedro Jalles <jpedrojalles@gmail.com>
2024-08-07 14:18:58 -0300

    Commit inicial

--- src/main/java/med/voll/api/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/medico/MedicoRepository.java
@@ -6,7 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
-
-
-
 }
